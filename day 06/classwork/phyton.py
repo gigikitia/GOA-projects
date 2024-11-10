467896e8d7d70f190227445077929bd6b6d34574
მარიამ ჩალოვი <kitiag334@gmail.com>
2024-11-10 22:54:18 +0400

    first commit

---
+++ day 06/classwork/phyton.py
@@ -0,0 +1,13 @@
+# მშობლების ასაკი
+father_age = 50
+mother_age = 45
+
+# გამოთვლა, რამდენი წლის იქნებიან ისინი 20 წელში
+father_age_in_20_years = father_age + 20
+mother_age_in_20_years = mother_age + 20
+
+print("მამა 20 წელში:", father_age_in_20_years)
+print("დედა 20 წელში:", mother_age_in_20_years)
+
+მამა 20 წელში: 70
+დედა 20 წელში: 65
