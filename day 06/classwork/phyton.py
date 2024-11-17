93e90563ec5da66c581e2bb035cddb2430fc6e69
მარიამ ჩალოვი <kitiag334@gmail.com>
2024-11-17 13:29:41 +0400

    first commit

--- day 06/classwork/phyton.py
+++ day 06/classwork/phyton.py
@@ -11,3 +11,36 @@ print("დედა 20 წელში:", mother_age_in_20_years)
 
 მამა 20 წელში: 70
 დედა 20 წელში: 65
+
+
+საკლასო დავალება
+
+1name = "Zuka"     
+user-name = "Ana" 
+for = 10                   
+class = "Math"             
+print(userAge) 
+age = 25
+age = age + "years"          
+Name = "Zuka"
+print(name)                  
+full name = "Zuka K"         
+def = "Function"           
+2userAge = 20
+
+
+name = "Zuka"            
+user_name = "Ana"        
+age = 25              
+age_in_years = str(age) + " years"  
+full_name = "Zuka K"     
+subject = "Math"         
+user_age = 20           
+
+
+print("User's name:", user_name)  
+print("User's age:", user_age)    
+print("Subject:", subject)        
+print("Age in years:", age_in_years)  
+
+
