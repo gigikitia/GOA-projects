915d27855e998323791db6f1930cb9e5a7207fef
მარიამ ჩალოვი <kitiag334@gmail.com>
2024-12-22 19:32:32 +0400

    first commit

--- day 10/classwork/phyton.py
+++ day 10/classwork/phyton.py
@@ -1,1 +1,22 @@
-print("no classwork")
+
+name = input("gigi ")
+surname = input("kitia ")
+age = input("15" )
+number1 = input("16")
+number2 = input("18 ")
+
+
+print( name)
+print(surname)
+print(age)
+print( number1)
+print( number2)
+
+
+
+num1 = float(input("18"))
+num2 = float(input("19 "))
+
+
+sum_result = num1 + num2
+print("ორივე რიცხვის ჯამი:", sum_result)
