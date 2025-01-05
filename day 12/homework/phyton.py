6b8ac4c17936b20cf0c3ec666707a6aa03fec0c0
მარიამ ჩალოვი <kitiag334@gmail.com>
2025-01-05 21:15:56 +0400

    first commit

---
+++ day 12/homework/phyton.py
@@ -0,0 +1,35 @@
+# მომხმარებლისგან რიცხვების მიღება
+num1 = float(input("შეიყვანეთ პირველი რიცხვი (num1): "))
+num2 = float(input("შეიყვანეთ მეორე რიცხვი (num2): "))
+
+# ლოგიკური ოპერატორები
+print(f"{num1} > {num2}: {num1 > num2}")
+print(f"{num1} < {num2}: {num1 < num2}")
+print(f"{num1} >= {num2}: {num1 >= num2}")
+print(f"{num1} <= {num2}: {num1 <= num2}")
+print(f"{num1} != {num2}: {num1 != num2}")
+print(f"{num1} == {num2}: {num1 == num2}")
+
+
+
+# მომხმარებლისგან სიგანის (width) და სიმაღლის (height) მიღება
+width = float(input("შეიყვანეთ მართკუთხედის სიგანე (width): "))
+height = float(input("შეიყვანეთ მართკუთხედის სიმაღლე (height): "))
+
+# პერიმეტრის გამოთვლა
+perimeter = 2 * (width + height)
+
+
+# შედეგის ჩვენება
+print(f"მართკუთხედის პერიმეტრი არის: {perimeter}")
+
+
+# მომხმარებლისგან სახელი (name) და რიცხვი (num) მიღება
+name = input("შეიყვანეთ თქვენი სახელი: ")
+num = int(input("შეიყვანეთ რიცხვი: "))
+
+# name გამრავლება num-ზე
+result = name * num
+
+# შედეგის პრინტინგი
+print(f"გამოყენებული სახელი {num} ჯერ: {result}")
