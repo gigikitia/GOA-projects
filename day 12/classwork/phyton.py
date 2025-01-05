6bc050c39442803c4e2e982ba7fec97971d6b19c
მარიამ ჩალოვი <kitiag334@gmail.com>
2025-01-05 22:44:56 +0400

    first commit

---
+++ day 12/classwork/phyton.py
@@ -0,0 +1,10 @@
+print(5 > 3)  # True, რადგან 5 დიდია 3-ზე
+print(2 > 10) # False, რადგან 2 პატარაა 10-ზე
+print(7 > 7)  # False, რადგან 7 არ არის დიდი 7-ზე
+print(10 > 9) # True, რადგან 10 დიდი არის 9-ზე
+print(0 > -1) # True, რადგან 0 დიდი არის -1-ზე
+
+print(True and True)   # True, რადგან ორივე არის True
+print(True and False)  # False, რადგან ერთი False-ია
+print(False and True)  # False, რადგან ერთი False-ია
+print(False and False) # False, რადგან ორივე False-ია
